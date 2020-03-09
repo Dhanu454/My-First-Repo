24f220a72444ad0b72bd181d381a86ea94d6c546
Dhanu454 <dhanu.454@gmail.com>
2020-03-09 09:48:34 +0000

    addding nag file to central repo

---
+++ nag.java
@@ -0,0 +1,4 @@
+hi this is a new file created under new branch called manoj 
+now add this file to index ad then commit and push it into manoj
+From there merge this branch to dhanu branch from local repository and again merge dhanu branch into master
+check whether above codition satisfies works or not
