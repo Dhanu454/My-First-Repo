9d0662ba878baeffde723c9c249beab436d18de1
Dhanu454 <dhanu.454@gmail.com>
2020-03-09 10:29:09 +0000

    dhanu adding to edit files from direct commmit

--- nag.java
+++ nag.java
@@ -2,3 +2,11 @@ hi this is a new file created under new branch called manoj
 now add this file to index ad then commit and push it into manoj
 From there merge this branch to dhanu branch from local repository and again merge dhanu branch into master
 check whether above codition satisfies works or not
+
+
+
+its worked 
+
+
+changes:- this file is first pulled from branch manoj 
+and then from dhanu it is pulled to master branch
